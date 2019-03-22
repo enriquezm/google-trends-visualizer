4e52bf78d13c37756173c113e830744537a4b722
Myles Enriquez <enri.myles@gmail.com>
2019-03-22 16:25:02 -0700

    Add component generation logic.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, { Component } from 'react';
 import Box from './components/Box';
 import logo from './logo.svg';
 import './App.css';
+import AnimalListData from './animal_names.json';
+import Colors from './colors.json';
 
 
 
@@ -12,15 +14,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gridSize: 5
+      gridSize: 25, //default requirement
+      animals: []
     }
+    this.createZoo = this.createZoo.bind(this);
   }
+  componentDidMount() {
+    this.createZoo();
+  }
+  createZoo(){
+    // Creates a concatenated string of animal box components based on grid size
+    const animalArray = [];
+    for(let i = 0; i < this.state.gridSize; i++) {
+      const animalNumber = Math.floor((Math.random() * this.state.gridSize) + 1); // number between 1 and gridSize (not inclusive)
+      const colorNumber = Math.floor((Math.random() * 4) + 0);
+      animalArray.push(
+        <Box bgColor={Colors[colorNumber]} text={AnimalListData[animalNumber]}/>
+      );
+    }
+    console.log(animalArray);
+    this.setState({
+      animals: animalArray
+    });
+  }
+    
   render() {
     return (
       <div className="grid">
-        <Box bgColor="salmon" text="Cat"/>
-        <Box bgColor="purple" text="Cat"/>
-        <Box bgColor="green" text="Cat"/>
+        {
+          this.state.animals.map((animal) => animal)
+        }
       </div>
     );
   }

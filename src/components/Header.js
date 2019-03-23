4afd66b520154c568e8e9db35989330ecd2e5c2b
Myles Enriquez <enri.myles@gmail.com>
2019-03-22 21:53:01 -0700

    Create header.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+    <header>
+        <h1>Animal Trends Visualizer</h1>
+        <p>By <a href="https://github.com/enriquezm">Myles Enriquez</a></p>
+    </header>
+)
+
+export default Header;

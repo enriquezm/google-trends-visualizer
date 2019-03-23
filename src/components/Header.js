a3acfe813e1b47cf165c739c565059dbed489ca9
Myles Enriquez <enri.myles@gmail.com>
2019-03-22 21:56:11 -0700

    Add source code link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => (
     <header>
         <h1>Animal Trends Visualizer</h1>
-        <p>By <a href="https://github.com/enriquezm">Myles Enriquez</a></p>
+        <p>By <a href="https://github.com/enriquezm">Myles Enriquez</a>. View project's <a href="https://github.com/enriquezm/google-trends-visualizer">source code</a>.</p>
     </header>
 )
 
